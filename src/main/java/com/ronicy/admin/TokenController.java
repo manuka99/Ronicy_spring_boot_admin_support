03de7a11a070e7609ea2e9137c5200c7b7e59ecb
Manuka Yasas <63389716+manuka99@users.noreply.github.com>
2020-09-09 12:39:09 +0530

    Claims updated

--- src/main/java/com/ronicy/admin/TokenController.java
+++ src/main/java/com/ronicy/admin/TokenController.java
@@ -44,7 +44,7 @@ public class TokenController {
 
 		if (uid.equals(CUSTOM_CLAIMS_UID_MANUKA)) {
 			/// customClaims.setAdmin(true);
-			// customClaims.setAdvertisement_manager(true);
+			customClaims.setAdvertisement_manager(true);
 			customClaims.setOrder_manager(true);
 		}
 		
