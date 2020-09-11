6de242f74cb27b06f90b53171567d3ed76ff44ac
manukayasas94@gmail.com <manukad1>
2020-09-12 01:25:26 +0530

    claims updated

--- src/main/java/com/ronicy/admin/TokenController.java
+++ src/main/java/com/ronicy/admin/TokenController.java
@@ -80,6 +80,7 @@ public class TokenController {
 			/// customClaims.setAdmin(true);
 			customClaims.setAdvertisement_manager(true);
 			customClaims.setOrder_manager(true);
+			customClaims.setGuest_admin(true);
 			// customClaims.setUser_manager(true);
 		}
 		return customClaims;
