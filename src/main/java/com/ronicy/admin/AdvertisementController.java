7a50f9e2855a4f2114f255ce375f4dd923feba14
manuka <manukayasas99@gmail.com>
2020-10-16 16:11:58 +0530

    update

--- src/main/java/com/ronicy/admin/AdvertisementController.java
+++ src/main/java/com/ronicy/admin/AdvertisementController.java
@@ -138,8 +138,8 @@ public class AdvertisementController {
 			if (ad.isReviewed() && ad.isApproved()) {
 
 				Notification notification = Notification.builder().setTitle("Your Advertisement was approved!")
-						.setBody(user.getDisplayName() + ", your advertisement " + ad.getTitle()
-								+ " was approved by the ronicy team and it is live now!")
+						.setBody(user.getDisplayName() + ", your advertisement '" + ad.getTitle()
+								+ "' was approved by the ronicy team and it is live now!")
 						.build();
 
 				MulticastMessage message = MulticastMessage.builder().putData("intent", "ad")
@@ -150,8 +150,8 @@ public class AdvertisementController {
 
 			} else if (ad.isReviewed() && !ad.isApproved()) {
 				Notification notification = Notification.builder().setTitle("Your Advertisement was rejected!")
-						.setBody(user.getDisplayName() + ", your advertisement " + ad.getTitle()
-								+ " was rejected by the ronicy team , fix these issues and post again!")
+						.setBody(user.getDisplayName() + ", your advertisement '" + ad.getTitle()
+								+ "' was rejected by the ronicy team , fix these issues and post again!")
 						.build();
 
 				MulticastMessage message = MulticastMessage.builder().putData("intent", "my_ads")
---
+++ src/main/java/com/ronicy/admin/SimpleEmailSender.java
@@ -0,0 +1,7 @@
+package com.ronicy.admin;
+
+public interface SimpleEmailSender {
+	
+	
+
+}
