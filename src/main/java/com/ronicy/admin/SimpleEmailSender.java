fa34b52c3376b52cef0a7036e379fd7f45403329
manuka <manukayasas99@gmail.com>
2020-10-16 17:12:14 +0530

    email updated

--- src/main/java/com/ronicy/admin/SimpleEmailSender.java
+++ src/main/java/com/ronicy/admin/SimpleEmailSender.java
@@ -1,16 +1,13 @@
 package com.ronicy.admin;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -54,7 +51,7 @@ public interface SimpleEmailSender {
 			// sends the e-mail
 			Transport.send(msg);
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		}
 
 	}
